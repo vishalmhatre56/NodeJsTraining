ba12ab8a2426a21aa264506a844b7cd62bf810d6
Vishal Mhatre <v.mhatre56@gmail.com>
2019-02-14 18:20:23 +0530

    lec 12 completed

--- notes-node/app.js
+++ notes-node/app.js
@@ -2,10 +2,16 @@ console.log("starting app");
 
 // const fs = require("fs");
 // const os = require("os");
-const notes = require("./notes.js") 
+// const notes = require("./notes.js")
+const _ = require('lodash');
 
-const sum = notes.add(9,-2);
-console.log(sum);
+// console.log(_.isString(true));
+// console.log(_.isString("stringsd"))
+const filterdArray = _.uniq(["The","Vish",1,"Vish",1,2,3,4,"Pro"]);
+console.log(filterdArray);
+
+// const sum = notes.add(9,-2);
+// console.log(sum);
 
 // const user=os.userInfo();
 // fs.appendFile('greetings.txt',`Hello ${user.username}!`);
