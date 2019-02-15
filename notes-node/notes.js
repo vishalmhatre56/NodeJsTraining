f7616026a2b1762813dd102c1411381f161ea759
Vishal Mhatre <v.mhatre56@gmail.com>
2019-02-15 11:27:54 +0530

    lec 16 completed

--- notes-node/notes.js
+++ notes-node/notes.js
@@ -1,7 +1,25 @@
 console.log("starting notes.js");
-// var notes
+const fs = require('fs');
+
 var addNote = (title, body) => {
-    console.log("Adding note,",title,body);
+    var notes = [];
+    var note = {
+        title,
+        body
+    }
+
+    try{
+        var noteString = fs.readFileSync("notes-data.json");
+        notes = JSON.parse(noteString); 
+    }catch(e){
+
+    }
+    var duplicateNotes = notes.filter((n)=> n.title===title);
+    
+    if(duplicateNotes.length===0){
+        notes.push(note);
+        fs.writeFileSync("notes-data.json", JSON.stringify(notes));    
+    }
 }
 var getAll = () => {
     console.log("list..");
